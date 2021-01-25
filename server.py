47e3e7bbad4b665761f41488ac27288dbff5d4c2
Jhoan Ortiz S <johanjesus133@hotmail.com>
2021-01-24 22:02:11 -0500

    cors enabled

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_restful import Api
 from Apis.PasswordGenerator.passGenerator_API import passwordGeneratorAPI
 from Apis.defaultPage import DefaultPage
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
